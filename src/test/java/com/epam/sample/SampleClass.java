a9580ae33e8a0b6c3f24f050d679a8c39c6f4af1
Pratap Chandra Ghosh <sweetpratap.g@gmail.com>
2026-02-14 16:04:41 +0530

    first commit

---
+++ src/test/java/com/epam/sample/SampleClass.java
@@ -0,0 +1,16 @@
+package com.epam.sample;
+
+import org.testng.annotations.Test;
+
+public class SampleClass {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("This is a sample test.");
+    }
+
+    @Test
+    public void anotherSampleTest() {
+        System.out.println("This is another sample test.");
+    }
+}
